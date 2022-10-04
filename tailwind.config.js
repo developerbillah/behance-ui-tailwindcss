a843033e39d8726b0c1258b3ee9d985543d8f60d
Md Masum Billah <masum@deligram.com>
2022-10-04 22:39:15 +0600

    Project Setup & Behance Header Clone Using Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        notoSans : ['Noto Sans', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
